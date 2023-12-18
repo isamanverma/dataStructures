5117d59375cca3ecd6cf6a4c10f3aef1f909efb0
amankumarverma <theamankumarverma@gmail.com>
2023-12-18 23:11:37 +0530

    swapping alternate elements of an aray

---
+++ leetcode/swapAlternate.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void swapAlternate(int arr[], int size)
+{
+    if (size % 2 == 0)
+        for (int i = 0; i < size; i = i + 2)
+            swap(arr[i], arr[i + 1]);
+    else
+        for (int i = 0; i < size - 1; i = i + 2)
+            swap(arr[i], arr[i + 1]);
+}
+
+int main(void)
+{
+    int arr[] = {10, 23, 12, 33, 45, 63, 62};
+    int size = sizeof(arr) / sizeof(arr[1]);
+    cout << "Array before alternate swap: ";
+    for (int i : arr)
+        cout << i << " ";
+
+    cout << endl;
+
+    swapAlternate(arr, size);
+    cout << "Array after alternate swap: ";
+    for (int i : arr)
+        cout << i << " ";
+    return 0;
+}
