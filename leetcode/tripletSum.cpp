f2937c807bf7de9e18565d464d58e86513616b06
amankumarverma <theamankumarverma@gmail.com>
2023-12-20 00:42:58 +0530

    method 1: triplet sum

--- leetcode/tripletSum.cpp
+++ leetcode/tripletSum.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm> // for sort
 using namespace std;
 
 vector<vector<int>> tripletSum(vector<int> &arr, int s)
 {
     vector<vector<int>> ans;
+    set<vector<int>> uniqueTriplets;
     int size = arr.size();
+
     for (int i = 0; i < size; i++)
     {
         for (int j = i + 1; j < size; j++)
@@ -14,29 +18,30 @@ vector<vector<int>> tripletSum(vector<int> &arr, int s)
             {
                 if (arr[i] + arr[j] + arr[k] == s)
                 {
-                    vector<int> triplet;
-                    triplet.emplace_back(arr[i]);
-                    triplet.emplace_back(arr[j]);
-                    triplet.emplace_back(arr[k]);
-                    ans.push_back(triplet);
+                    vector<int> triplet = {arr[i], arr[j], arr[k]};
+                    sort(triplet.begin(), triplet.end());
+                    uniqueTriplets.insert(triplet);
                 }
             }
         }
     }
-    sort(ans.begin(), ans.end());
+    ans.assign(uniqueTriplets.begin(), uniqueTriplets.end());
+
     return ans;
 }
 
 int main(void)
 {
-    vector<int> arr = {1, 2, 3, 4, 5, 6};
-    int s = 12;
+    vector<int> arr = {1, 2, 3, 1, 2, 3};
+    sort(arr.begin(), arr.end());
+    int s = 6;
 
-    for (vector<int> pair : tripletSum(arr, s))
+    for (vector<int> triplet : tripletSum(arr, s))
     {
-        for (int i : pair)
+        for (int i : triplet)
             cout << i << " ";
         cout << endl;
     }
+
     return 0;
 }
