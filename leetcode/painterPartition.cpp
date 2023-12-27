1f91d27ef6e1ba30d3e76c8dbdfec3dc4cd34c5a
amankumarverma <theamankumarverma@gmail.com>
2023-12-28 01:58:48 +0530

    same as book allocation

--- leetcode/painterPartition.cpp
+++ leetcode/painterPartition.cpp
@@ -1,14 +1,3 @@
-/*
-arr of Books = {50, 60, 80}; -- each element is the number of pages of the book at ith place
-no. of students = m
-Condition of allocation:
-1. Each students gets atleast one book
-2. Each book gets allocated
-3. Book allocation should be in continuous manner
-
-allocate the books in such a way that max. no. of pages allocated to a student is minimum
-*/
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -18,28 +7,29 @@ using namespace std;
 bool isPossibleSol(vector<int> &arr, int m, int mid)
 {
     int n = arr.size();
-    int student = 1;
-    int pageSum = 0;
+    int painter = 1;
+    int areaSum = 0;
     for (int i = 0; i < n; i++)
     {
-        if (pageSum + arr[i] <= mid)
+        if (areaSum + arr[i] <= mid)
         {
-            pageSum += arr[i];
+            areaSum += arr[i];
         }
         else
         {
-            student++;
-            if (student > m || arr[i] > mid)
-            {
+            painter++;
+            if (painter > m || arr[i] > mid)
                 return false;
+            else
+            {
+                areaSum = arr[i];
             }
-            pageSum = arr[i];
         }
     }
     return true;
 }
 
-int allocate(vector<int> &arr, int m)
+int partition(vector<int> &arr, int m)
 {
     int n = arr.size();
     int start = 0;
@@ -69,9 +59,8 @@ int allocate(vector<int> &arr, int m)
 
 int main(void)
 {
-    vector<int> arr = {10, 20, 30, 40};
-    // vector<int> arr = {5, 5, 5, 5};
-    int m = 2;
-    cout << allocate(arr, m) << endl;
+    vector<int> arr = {5, 5, 5, 5};
+    int k = 2;
+    cout << partition(arr, k) << endl;
     return 0;
 }
